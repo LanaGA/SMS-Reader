821aa2568969163e242a84cbc8a89fc051f629cb
LanaGA <l.galieva@innopolis.ru>
2020-07-10 13:01:42 +0300

    everything fine, except wrong encoding

--- app/src/main/java/com/rsf/sms_reader/MainActivity.kt
+++ app/src/main/java/com/rsf/sms_reader/MainActivity.kt
@@ -1,52 +1,42 @@
 package com.rsf.sms_reader
 
 import android.Manifest
+import android.content.IntentFilter
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.telephony.SmsManager
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    var smsList: ArrayList<String>? = null
+    private val smsReceiver by lazy { SMSReceiver() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //  listView = findViewById(R.id.idList);
-        check_sms.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED)
-                showContacts()
-            else
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_SMS), PERMISSION_REQUEST_CODE)
-        }
+        if (permissionCheck("RECEIVE_SMS") == PackageManager.PERMISSION_GRANTED) {
+            registerReceiver(smsReceiver, IntentFilter("android.provider.Telephony.SMS_RECEIVED"))
+        } else
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECEIVE_SMS), PERMISSION_REQUEST_CODE)
+
     }
+
 
     private fun permissionCheck(permission: String): Int {
         return ContextCompat.checkSelfPermission(this, "Manifest.permission.$permission")
     }
 
-    private fun showContacts() {
-        val uri = Uri.parse("content://sms/")
-        val contentResolver = contentResolver
-        val sms = "address='$REQ_NUMBER'"
-        val cursor = contentResolver.query(uri, arrayOf("_id", "body"), sms, null, null) ?: return
-        println(cursor.count)
+    override fun onResume() {
+        super.onResume()
 
-        while (cursor.moveToNext()) {
-            val body = cursor.getString(cursor.getColumnIndex("body"))
-            Toast.makeText(this, body, Toast.LENGTH_LONG).show()
-        }
-        cursor.close()
+        if (permissionCheck("RECEIVE_SMS") == PackageManager.PERMISSION_GRANTED) {
+            registerReceiver(smsReceiver, IntentFilter("android.provider.Telephony.SMS_RECEIVED"))
+        } else
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECEIVE_SMS), PERMISSION_REQUEST_CODE)
     }
 
     companion object {
-        private const val REQ_NUMBER = "900"
         private const val PERMISSION_REQUEST_CODE = 100
     }
 }
--- app/src/main/java/com/rsf/sms_reader/SMSReceiver.kt
+++ app/src/main/java/com/rsf/sms_reader/SMSReceiver.kt
@@ -7,16 +7,22 @@ import android.os.Build
 import android.os.Bundle
 import android.telephony.SmsMessage
 import android.widget.Toast
-import java.util.*
+import org.json.JSONObject
+import java.io.DataOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
 
 
-private class SMSReceiver : BroadcastReceiver() {
+internal class SMSReceiver : BroadcastReceiver() {
     companion object {
-        private const val REQ_NUMBER = "+79997585058"
+        private const val REQ_NUMBER = "900"
     }
+
     private var bundle: Bundle? = null
     private var currentSMS: SmsMessage? = null
     private var message: String? = null
+
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == "android.provider.Telephony.SMS_RECEIVED") {
             bundle = intent.extras
@@ -26,14 +32,47 @@ private class SMSReceiver : BroadcastReceiver() {
                     for (aObject in pduObjects) {
                         currentSMS = aObject?.let { getIncomingMessage(it, bundle!!) }
                         val senderNo = currentSMS!!.displayOriginatingAddress
-                        if (senderNo != REQ_NUMBER) continue
+                        if (senderNo != REQ_NUMBER) {
+                            Toast.makeText(context, "other message", Toast.LENGTH_LONG).show()
+                            continue
+                        }
                         message = currentSMS!!.displayMessageBody
-                        Toast.makeText(context, "senderNum: $senderNo :\n message: $message", Toast.LENGTH_LONG).show()
+                        //Toast.makeText(context, "message: $message", Toast.LENGTH_LONG).show()
+                        try {
+                            sendPost("http://34.66.156.110/send_message", message.toString())
+                            Toast.makeText(context, "Success", Toast.LENGTH_LONG).show()
+                        } catch (e: Exception) {
+                            Toast.makeText(context, "Sending error", Toast.LENGTH_LONG).show()
+                        }
                     }
                     abortBroadcast()
                 }
             }
         }
+    }
+
+    private fun sendPost(urlAddress: String, message: String) {
+        val thread = Thread(Runnable {
+            try {
+                val url = URL(urlAddress)
+                val conn = url.openConnection() as HttpURLConnection
+                conn.requestMethod = "POST"
+                conn.setRequestProperty("Content-Type", "application/json; charset=utf-8")
+                conn.setRequestProperty("Accept", "application/json")
+                conn.doOutput = true
+                conn.doInput = true
+                val jsonParam = JSONObject()
+                jsonParam.put("message", URLEncoder.encode(message, "utf-8"))
+                val os = DataOutputStream(conn.outputStream)
+                os.writeBytes(jsonParam.toString())
+                os.flush()
+                os.close()
+                conn.disconnect()
+            } catch (e: java.lang.Exception) {
+                e.printStackTrace()
+            }
+        })
+        thread.start()
     }
 
     private fun getIncomingMessage(aObject: Any, bundle: Bundle): SmsMessage? {
